8266b0165d2c97ace840de78f25e239a41d3e582
Takahiro <tkhr_621@docomo.ne.jp>
2021-11-03 23:29:42 +0900

    ヘッダー修正

--- pages/youtube.tsx
+++ pages/youtube.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 import Layout from '../components/layouts/Layout'
 
-const Campany = () => (
+const Youtube = () => (
   <Layout title="About | Next.js + TypeScript Example">
-    <h1>Campany</h1>
+    <h1>Youtube</h1>
     <p>This is the company page</p>
     <p>
       <Link href="/">
@@ -13,4 +13,4 @@ const Campany = () => (
   </Layout>
 )
 
-export default Campany
+export default Youtube
