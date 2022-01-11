6584878c183f3d82350ce8ecde5eee2479a4ae7e
YumaNakamura <yuma.0309@icloud.com>
2022-01-11 20:03:08 +0900

    変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Layout from '../components/layouts/Layout'
 
 const IndexPage = () => (
   <Layout title="Nansuta Home Page">
-    <h1>Hello👋👋👋👋👋👋</h1>
+    <h1>Hello👋👋👋</h1>
   </Layout>
 )
 
